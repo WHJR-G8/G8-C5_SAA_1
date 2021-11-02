6d786a27d4a4fae875a6bc3237e738cd91bf8419
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-11-02 12:11:52 +0530

    Update Sol_SAA_1.py

--- Sol_SAA_1.py
+++ Sol_SAA_1.py
@@ -3,13 +3,13 @@ age = int(input("Enter a valid age: "))
 if age < 0:
     print('Not a valid age')
     
-elif (0 < age) < 18:
+elif 12 < age < 18:
     print('You are too young')
     
 elif age == 18:
     print('You have just entered adulthood')
     
-elif (19 < age) < 21:
+elif 18 < age < 21:
     print('You are a bachelor')
     
 else:
